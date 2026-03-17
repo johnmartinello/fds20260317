c2ba4983ebfa5f879de7a618121b260eb846cf3d
Marcelo H. Yamaguti <yamaguti@pucrs.br>
2026-03-17 12:32:09 +0000

    Setup inicial

---
+++ presenca20250317/src/App.java
@@ -0,0 +1,10 @@
+public class App {
+    public static void main(String[] args) {
+        System.out.println("FUNDAMENTOS DE DESENVOLVIMENTO DE SOFTWARE");
+        System.out.println("Presenca da aula 17/3/2026: ");
+
+        // Insira uma linha que mostre o seu nome na tela
+
+
+    }
+}
