9ce15352a8a15196688dd9813df4ca125378d96f
João Lucas Martinello <100368900+johnmartinello@users.noreply.github.com>
2026-03-17 22:09:13 -0300

    commitei

--- presenca20250317/src/App.java
+++ presenca20250317/src/App.java
@@ -4,6 +4,7 @@ public class App {
         System.out.println("Presenca da aula 17/3/2026: ");
         // Insira uma linha que mostre o seu nome na tela
         System.out.println("Natan Stallivieri da Silva");
+        System.out.println("João lucas martinello de oliveira");
         // Iohanna Sofia Dietz Maldaner :)
     }
 }
